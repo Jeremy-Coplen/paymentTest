e8763024dcfef376b0b18461507e671ad6bb8ba6
Jeremy Coplen <jeremycoplen.jc@gmail.com>
2018-12-26 14:08:24 -0700

    Styling changes and continued stripe setup

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Elements, StripeProvider } from "react-stripe-elements"
 
 import CartItem from "../CartItem/CartItem"
 import Payment from "../Payment/Payment"
@@ -8,28 +9,32 @@ import "./Cart.css"
 function Cart(props) {
     const className = props.cartShow ? "cart_display" : "cart_display_none"
     const buttonClass = props.cartArray.length ? "purchase_button" : "purchase_button_none"
-    if(props.cartArray.length) {
+    if (props.cartArray.length) {
         var cartItems = props.cartArray.map(cartItem => {
             return (
-                <CartItem  
-                key={cartItem.id} 
-                cartItem={cartItem} 
-                addToCart={props.addToCart} 
-                removeFromCart={props.removeFromCart}/>
+                <CartItem
+                    key={cartItem.id}
+                    cartItem={cartItem}
+                    addToCart={props.addToCart}
+                    removeFromCart={props.removeFromCart} />
             )
         })
     }
     return (
-        <div className={className} style={{width: props.cartWidth}}>
-        <button className="close_button" onClick={() => props.closeCart()}>X</button>
-        {cartItems}
-        <p>Subtotal: {props.subTotal.toFixed(2)}</p>
-        <p>Total: {props.total.toFixed(2)}</p>
-        <button className={buttonClass}
-        onClick={() => props.togglePaymentShow()}>Confirm Purchase</button>
-        <Payment 
-        paymentShow={props.paymentShow}
-        togglePaymentShow={props.togglePaymentShow} />
+        <div className={className} style={{ width: props.cartWidth }}>
+            <button className="close_button" onClick={() => props.closeCart()}>X</button>
+            {cartItems}
+            <p>Subtotal: {props.subTotal.toFixed(2)}</p>
+            <p>Total: {props.total.toFixed(2)}</p>
+            <button className={buttonClass}
+                onClick={() => props.togglePaymentShow()}>Confirm Purchase</button>
+            <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+                <Elements>
+                    <Payment
+                        paymentShow={props.paymentShow}
+                        togglePaymentShow={props.togglePaymentShow} />
+                </Elements>
+            </StripeProvider>
         </div>
     )
 }
--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,17 +1,41 @@
-import React from "react"
+import React, { Component } from "react"
+import { CardElement, injectStripe } from "react-stripe-elements"
+import axios from "axios"
 
 import "./Payment.css"
 
-function Payment(props) {
-    const className = props.paymentShow ? "payment" : "payment_none"
-    return (
-        <div className={className}>
-            <div className="payment_content">
-                    <button onClick={() => props.togglePaymentShow()}>X</button>
-                    <p>payment</p>
+class Payment extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            complete: false
+        }
+    }
+
+    submit = async () => {
+        let {token} = await this.props.stripe.createToken({name: "Name"})
+        let res = await axios.post("/api/payment", {token: token.id})
+
+        if(res.ok) {
+            console.log("Purchase complete")
+        }
+    }
+
+    render() {
+        const className = this.props.paymentShow ? "payment" : "payment_none"
+        return (
+            <div className={className}>
+                <div className="payment_content">
+                    <button onClick={() => this.props.togglePaymentShow()}>X</button>
+                    <div>
+                        <CardElement />
+                        <button>Submit</button>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default Payment
+export default injectStripe(Payment)
